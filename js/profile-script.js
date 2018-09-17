e7d8eab3ff19f0ef3088b6b87ead335f88e756bd
Mohamed Abdelnaeem <melnaeem00@gmail.com>
2018-09-17 18:32:54 +0200

    change pp and cover feature - unfinished

--- js/profile-script.js
+++ js/profile-script.js
@@ -15,7 +15,6 @@ $(document).ready(function(){
     $('.createMsgModal').on('show.bs.modal', function (e) {
         var $trigger = $(e.relatedTarget);
         
-        
         if($trigger.hasClass('send-btn')){
             msgReciverOption.css("display","none");
             profileReciver.css("display","block");
@@ -26,8 +25,59 @@ $(document).ready(function(){
             msgReciverOption.css("display","block");
             profileReciver.css("display","none");
         }
+    });
+    
+    var inputImg = $('.info input[type="file"]');
+    var imageModal = $('#changeImgModal');
+    
+    inputImg.change(function(){
+        //console.log('change Happen');
+        
+        var input = this;
+        
+        imageModal.modal('show');
+        
+        var reader = new FileReader();
+        if (input.files && input.files[0]){
+            reader.onload = function(e){
+                imageModal.find('img').attr('src', e.target.result);
+                console.log($(input).is("#inputProfilePic"));
+                /*
+                if($(input).is("#inputProfilePic")){
+                    imageModal.find('img').attr('title','pp'); 
+                }
+                */
+            }
+            reader.readAsDataURL(input.files[0])
 
+        }
         
     });
+    $(imageModal).find('#approve').click(function(){    
+        console.log('approved');
+    })
+    /*
+    $('#changeImgModal #approve').click(function(e){
+            
+            console.log('function fired');
+            //console.log(input);
+            flag = true;
+            if (input.files && input.files[0] && flag){
+                
+                reader.onload = function(e){
+                    console.log(imageModal.find('img'));
+                    if($(input).attr('name') == 'coverInput'){
+                        $('.cover').attr('src', e.target.result); 
+                    }else if($(input).attr('name') == 'profilePicInput'){
+                        $('.profile').attr('src', e.target.result);
+                    }
+            }
+                reader.readAsDataURL(input.files[0])
+            }
+        })
+      */  
+    
+    console.log(inputImg);
+    
     
 })
